bd46c4028963b462977d9d1ce390ab68b8ccfb5a
Artem Galay <iostema@mail.ru>
2022-08-30 00:00:14 +0300

    Create TipsView.

---
+++ Total Bill/Views/TipsView.swift
@@ -0,0 +1,78 @@
+//
+//  TipsView.swift
+//  Total Bill
+//
+//  Created by Артем Галай on 29.08.22.
+//
+
+import UIKit
+
+class TipsView: UIView {
+
+    private lazy var tipsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Persons"
+        label.textColor = #colorLiteral(red: 0.2454499006, green: 0.2894837558, blue: 0.3496103287, alpha: 1)
+        label.font = UIFont(name: "Avenir Next", size: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    } ()
+
+    let collectionView: UICollectionView = {
+        let collectionViewLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .red
+        return collectionView
+    } ()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupHierarchy()
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupHierarchy() {
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubview(tipsLabel)
+        addSubview(collectionView)
+        collectionView.register(TipsCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+    }
+
+    private func setDelegates() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            tipsLabel.topAnchor.constraint(equalTo: topAnchor, constant: 0),
+            tipsLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+
+            collectionView.topAnchor.constraint(equalTo: tipsLabel.bottomAnchor, constant: 2),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
+            collectionView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+
+    }
+}
+
+extension TipsView: UICollectionViewDelegate, UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        4
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? TipsCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
